a93c04a1605a6475fc5113ea7a6118a3bb8e2997
LoNGER-2111 <96116356+LoNGER-2111@users.noreply.github.com>
2023-06-07 00:12:22 +0700

    ellipsis pagination completed

--- src/components/pagination.tsx/index.tsx
+++ src/components/pagination.tsx/index.tsx
@@ -4,35 +4,95 @@ interface IPropsPagination {
   currentPage: number;
   setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
   totalPages: number;
+  displayRange?: number;
 }
 
 const Pagination: React.FC<IPropsPagination> = ({
   currentPage,
   setCurrentPage,
   totalPages,
+  displayRange = 3,
 }) => {
+  const getPageNumbers = () => {
+    const pageNumbers: (number | string)[] = [];
+    const totalPageRange = displayRange + 2;
+
+    if (totalPages <= totalPageRange) {
+      pageNumbers.push(
+        ...Array.from({ length: totalPages }, (_, index) => index + 1)
+      );
+    } else if (currentPage <= displayRange + 1) {
+      if (currentPage === 1) {
+        pageNumbers.push(
+          ...Array.from({ length: displayRange }, (_, index) => index + 1)
+        );
+      } else {
+        pageNumbers.push(
+          ...Array.from({ length: currentPage + 1 }, (_, index) => index + 1)
+        );
+      }
+      pageNumbers.push("...");
+      pageNumbers.push(totalPages);
+    } else if (currentPage > totalPages - displayRange - 1) {
+      pageNumbers.push(1);
+      pageNumbers.push("...");
+      if (currentPage === totalPages) {
+        pageNumbers.push(
+          ...Array.from(
+            { length: displayRange },
+            (_, index) => index + currentPage - displayRange + 1
+          )
+        );
+      } else {
+        pageNumbers.push(
+          ...Array.from(
+            { length: totalPages - currentPage + 2 },
+            (_, index) => index + currentPage - 1
+          )
+        );
+      }
+    } else {
+      pageNumbers.push(1);
+      pageNumbers.push("...");
+      pageNumbers.push(
+        ...Array.from(
+          { length: displayRange },
+          (_, index) => index + currentPage - 1
+        )
+      );
+      pageNumbers.push("...");
+      pageNumbers.push(totalPages);
+    }
+
+    return pageNumbers;
+  };
+
   return (
     <nav aria-label="Page navigation">
       <ul className="pagination justify-content-end m-0">
         <li
-          className={`page-item ${currentPage <= 1 && "disabled"}`}
+          className={`page-item ${currentPage <= 1 ? "disabled" : ""}`}
           onClick={() => {
             if (currentPage > 1) setCurrentPage(currentPage - 1);
           }}
         >
           <button className="page-link">Previous</button>
         </li>
-        {Array.from({ length: totalPages }).map((_, index) => (
+        {getPageNumbers().map((pageNumber, index) => (
           <li
             key={index}
-            className={`page-item ${currentPage === index + 1 && "active"}`}
-            onClick={() => setCurrentPage(index + 1)}
+            className={`page-item ${
+              currentPage === pageNumber ? "active" : ""
+            } ${pageNumber === "..." ? "disabled" : ""}`}
+            onClick={() => {
+              if (typeof pageNumber === "number") setCurrentPage(pageNumber);
+            }}
           >
-            <button className="page-link">{index + 1}</button>
+            <button className="page-link">{pageNumber}</button>
           </li>
         ))}
         <li
-          className={`page-item ${currentPage >= totalPages && "disabled"}`}
+          className={`page-item ${currentPage >= totalPages ? "disabled" : ""}`}
           onClick={() => {
             if (currentPage < totalPages) setCurrentPage(currentPage + 1);
           }}
